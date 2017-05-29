25842b1a6019d327b6dba51ceeef89d3ec094654
ANDRES <ANDRES@10.0.2.15>
2017-05-29 15:14:29 -0700

    Ventana Registro Usuario

--- src/java/AventronDP/OfertanteDP.java
+++ src/java/AventronDP/OfertanteDP.java
@@ -6,12 +6,30 @@
 package AventronDP;
 
 import AventronMD.OfertanteMD;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+import javax.naming.NamingException;
+import org.icefaces.ace.component.textentry.TextEntry;
 
 /**
  *
  * @author ANDRES
  */
-public class OfertanteDP {
+
+@ManagedBean
+@ViewScoped
+public class OfertanteDP implements Serializable {
     
     private int usuarioCI;
     private String usuarioNombre;
@@ -19,6 +37,20 @@ public class OfertanteDP {
     private String usuarioSexo;
     private String usuarioContrasena;
     private String ofertanteTipoLic;
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public OfertanteDP() {
+    }
+    
+    
 
     public OfertanteDP(int usuarioCI, String usuarioNombre, String usuarioTipo, String usuarioSexo, String usuarioContrasena, String ofertanteTipoLic) {
         this.usuarioCI = usuarioCI;
@@ -77,10 +109,92 @@ public class OfertanteDP {
         this.ofertanteTipoLic = ofertanteTipoLic;
     }
     
-    
+    private boolean verpaginas=false;
 
+    public boolean isVerpaginas() {
+        return verpaginas;
+    }
+
+    public void setVerpaginas(boolean verpaginas) {
+        this.verpaginas = verpaginas;
+    }
+
+    private boolean VerDialogo;
+
+    public boolean isVerDialogo() {
+        return VerDialogo;
+    }
+
+    public void setVerDialogo(boolean VerDialogo) {
+        this.VerDialogo = VerDialogo;
+    }
     
     
+    public void Insertar() throws NamingException, SQLException {
+        OfertanteMD nuevo = new OfertanteMD();
+        //UsuarioMD consulta = new UsuarioMD();
+        //codigoUsuario = consulta.ConsultaporParametrosNombre(nombreUsuario).getCodigousuarios();
+        //Actividad nueva = new Actividad(codigoActividad, codigoUsuario, codigoResultado, numeracionActividad, detalleActividad);
+        if(usuarioSexo.equals("Masculino"))
+        {
+            usuarioSexo = "M";
+        }
+        else usuarioSexo = "F";
+        OfertanteDP nuevoOf = new OfertanteDP(usuarioCI, usuarioNombre, usuarioTipo, usuarioSexo, usuarioContrasena, ofertanteTipoLic);
+        nuevo.Insertar(nuevoOf);
+        this.setError(nuevo.getError());
+        this.setVerDialogo(true);
+    }
+    
+    /*public void blurListener(AjaxBehaviorEvent event) {
+        TextEntry textEntry = (TextEntry) event.getComponent();
+        String value = textEntry.getValue().toString().trim();
+        String label = textEntry.getLabel();
+        if (value.equals("") || value.equalsIgnoreCase(label)) {
+            int index = severityMap.get(label);
+            String message;
+            if (value.equals("")) {
+                message = "Atención: El valor de " + label + " no es válido";
+            } else {
+                message = "Atención: El valor de " + label + " no es válido";
+            }
+            FacesMessage facesMessage = new FacesMessage((FacesMessage.Severity) FacesMessage.VALUES.get(index), message, message);
+            FacesContext.getCurrentInstance().addMessage(textEntry.getClientId(), facesMessage);
+        }
+        
+        private static HashMap<String, Integer> severityMap = new HashMap<String, Integer>() {
+        {
+            put("Codigo", 2);
+            put("Nombre", 2);
+            put("Contraseña", 2);
+        }
+    
+        
+        
+
+    };*/
+    
+    public void guardar() {
+        OfertanteMD usuario = new OfertanteMD();
+        OfertanteDP nueva = new OfertanteDP(usuarioCI, usuarioNombre, usuarioTipo, usuarioSexo, usuarioContrasena, ofertanteTipoLic);
+        usuario.Insertar(nueva);
+        this.setError(usuario.getError());
+        if (this.error.compareTo("Ingreso Exitoso") == 0) {
+            this.setUsuarioCI(0);
+            this.setOfertanteTipoLic("");
+            this.setUsuarioContrasena("");
+            this.setUsuarioNombre("");
+            this.setUsuarioSexo("");
+            this.setUsuarioTipo("");
+            
+            
+        }else{
+        this.setVerDialogo(true);
+        }
+        
+        this.setVerDialogo(true);
+
+    }
     
     
 }
--- src/java/AventronMD/OfertanteMD.java
+++ src/java/AventronMD/OfertanteMD.java
@@ -32,41 +32,59 @@ public class OfertanteMD {
     public void setError(String error) {
         this.error = error;
     }
-    
-    
-    private void Insertar(OfertanteDP ofertante)
-    {
-       try {
+
+    public void Insertar(OfertanteDP ofertante) {
+        try {
             DataSource DSActividad = this.getConnection0();
             Connection con = DSActividad.getConnection();
             Statement st = con.createStatement();
             //String CI = ofertante.getUsuarioCI();
             String Query = "";
-            Query = "select * from OFERTANTE where USUARIOCI='"+ofertante.getUsuarioCI()+"'";
+            Query = "select * from OFERTANTE where USUARIOCI='" + ofertante.getUsuarioCI() + "'";
             ResultSet rs = st.executeQuery(Query);
             if (rs.next()) {
-                error = "La Actividad que desea crear ya existe.";
+                error = "El CI ya esta asociado a un usuario.";
                 con.close();
                 st.close();
                 Query = "";
             } else {
-           /*Query = "insert into actividad  (CODIGORESULTADO, CODIGOUSUARIO, NUMERACIONACTIVIDAD, DETALLEACTIVIDAD) values('"
-                    + actividad.getCodigoResultado() + "','"
-                    + actividad.getCodigoUsuario() + "','"
-                    + actividad.getNumeracionActividad() + "','"
-                    + actividad.getDetalleActividad() + "')";
-            error = "Ingreso Exitoso..";*/
-            st.executeUpdate(Query);
-            con.close();
-            st.close();
+                Query = "insert into OFERTANTE  (USUARIOCI, USUARIONOMBRE, USUARIOTIPO, USUARIOSEXO, USUARIOCONTRASENA, OFERTANTETIPOLIC) values('"
+                        + ofertante.getUsuarioCI() + "','"
+                        + ofertante.getUsuarioNombre() + "','"
+                        + ofertante.getUsuarioTipo() + "','"
+                        + ofertante.getUsuarioSexo() + "','"
+                        + ofertante.getUsuarioContrasena() + "','"
+                        + ofertante.getOfertanteTipoLic() + "')";
+                error = "Ingreso Exitoso..";
+                st.executeUpdate(Query);
+                con.close();
+                st.close();
             }
         } catch (NamingException ex) {
-            //Logger.getLogger(Actividad.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ex.getExplanation());
         } catch (SQLException ex) {
-            //Logger.getLogger(Actividad.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ex.toString());
         }
     }
-    
+
+    public void Modificar(OfertanteDP ofertante) throws SQLException, NamingException {
+        DataSource DSProyecto = this.getConnection0();
+        Connection con = DSProyecto.getConnection();
+        Statement st = con.createStatement();
+        //actividad = actividad1;
+        String Query = "";
+        Query = "update OFERTANTE SET USUARIONOMBRE = '"
+                + ofertante.getUsuarioNombre() + "', USUARIOTIPO = '"
+                + ofertante.getUsuarioTipo() + "', USUARIOSEXO = '"
+                + ofertante.getUsuarioSexo() + "', USUARIOCONTRASENA = '"
+                + ofertante.getUsuarioContrasena() + "', OFERTANTETIPOLIC = '"
+                + ofertante.getOfertanteTipoLic() + "' where USUARIOCI='"
+                + ofertante.getUsuarioCI() + "'";
+        st.executeUpdate(Query);
+        con.close();
+        st.close();
+    }
+
     private DataSource getConnection0() throws NamingException {
         Context c = new InitialContext();
         return (DataSource) c.lookup("java:comp/env/connection0");
