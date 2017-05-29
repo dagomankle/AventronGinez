817c06dce13fbdb814d033c4d49c6864700958dc
dagom <dagom@Dago-mobil>
2017-05-29 11:56:32 -0500

    Se probo y funciona la coneccion a la base de datos

--- src/java/AventronMD/AutomovilMD.java
+++ src/java/AventronMD/AutomovilMD.java
@@ -7,33 +7,21 @@ package AventronMD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 /**
  *
  * @author dagom
  */
 public class AutomovilMD {
-    Connection conexion = null;
 
-    public Connection conectar()
-    {
-        try
-        {
-          Class.forName("oracle.jdbc.driver.OracleDriver");
-          conexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Aventron0", "system", "questo");
-          System.out.println("Conectado!!");
-        }catch(Exception ex)
-        {
-            System.out.println("ERROR:"+ex.getMessage());
-        }
-        return conexion;
+    private DataSource getConnection0() throws NamingException {
+        Context c = new InitialContext();
+        return (DataSource) c.lookup("java:comp/env/connection0");
     }
     
-    
-    public static void main (String args[])
-    {
-        AutomovilMD d = new AutomovilMD();
-        System.out.println(""+d.conectar());
-    }
     
 }
